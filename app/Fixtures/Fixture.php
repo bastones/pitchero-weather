7138b861462aba45aedb5f7b10fcbdcb3b9c6c0a
Ben Stones <ben@b3ns.com>
2019-09-18 16:02:57 +0100

    Eliminate the need for the `FixtureLocation` class

--- app/Fixtures/Fixture.php
+++ app/Fixtures/Fixture.php
@@ -3,20 +3,25 @@
 namespace PitcheroWeather\Fixtures;
 
 use Carbon\CarbonInterface;
-use PitcheroWeather\Contracts\FixtureLocationInterface;
+use PitcheroWeather\Contracts\CoordinatesInterface;
 use PitcheroWeather\Contracts\FixtureTeamInterface;
 
 class Fixture
 {
     /**
+     * @var string
+     */
+    protected $city;
+    
+    /**
+     * @var CoordinatesInterface
+     */
+    protected $coordinates;
+
+    /**
      * @var FixtureTeamInterface
      */
     protected $team;
-
-    /**
-     * @var FixtureLocationInterface
-     */
-    protected $location;
     
     /**
      * @var CarbonInterface
@@ -26,15 +31,37 @@ class Fixture
     /**
      * Fixture constructor.
      *
+     * @param string $city
+     * @param CoordinatesInterface $coordinates
      * @param FixtureTeamInterface $team
-     * @param FixtureLocationInterface $location
      * @param CarbonInterface $kickoff
      */
-    public function __construct(FixtureTeamInterface $team, FixtureLocationInterface $location, CarbonInterface $kickoff)
+    public function __construct(string $city, CoordinatesInterface $coordinates, FixtureTeamInterface $team, CarbonInterface $kickoff)
     {
+        $this->city = $city;
+        $this->coordinates = $coordinates;
         $this->team = $team;
-        $this->location = $location;
         $this->kickoff = $kickoff;
+    }
+
+    /**
+     * Get the city.
+     * 
+     * @return string
+     */
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    /**
+     * Get the coordinates.
+     * 
+     * @return CoordinatesInterface
+     */
+    public function getCoordinates(): CoordinatesInterface
+    {
+        return $this->coordinates;
     }
 
     /**
@@ -42,19 +69,9 @@ class Fixture
      * 
      * @return FixtureTeamInterface
      */
-    public function getTeam()
+    public function getTeam(): FixtureTeamInterface
     {
         return $this->team;
-    }
-
-    /**
-     * Get the location.
-     * 
-     * @return FixtureLocationInterface
-     */
-    public function getLocation()
-    {
-        return $this->location;
     }
 
     /**
--- app/Fixtures/FixtureLoader.php
+++ app/Fixtures/FixtureLoader.php
@@ -16,12 +16,12 @@ class FixtureLoader implements FixtureLoaderInterface
     public function load(): array
     {
         return array_map(function ($fixture) {
-            $teams = new FixtureTeam($fixture['team']['home'], $fixture['team']['away']);
+            $city = $fixture['location']['name'];
             $coordinates = new Coordinates($fixture['location']['lat'], $fixture['location']['lng']);
-            $location = new FixtureLocation($fixture['location']['name'], $coordinates);
+            $team = new FixtureTeam($fixture['team']['home'], $fixture['team']['away']);
             $kickoff = new Carbon($fixture['kickoff']);
 
-            return new Fixture($teams, $location, $kickoff);
+            return new Fixture($city, $coordinates, $team, $kickoff);
         }, $this->getData());
     }
 
